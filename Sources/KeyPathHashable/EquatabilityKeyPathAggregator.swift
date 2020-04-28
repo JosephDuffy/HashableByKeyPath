9b0cbba94521982af24cd5b67e14c24bed2a1467
Joseph Duffy <commits@josephduffy.co.uk>
2020-04-28 23:49:17 +0100

    Remove commented lines

--- Sources/KeyPathHashable/EquatabilityKeyPathAggregator.swift
+++ Sources/KeyPathHashable/EquatabilityKeyPathAggregator.swift
@@ -7,18 +7,6 @@ internal struct EquatabilityKeyPathAggregator<Root>: EquatableKeyPathConsumer {
     private var closures: [EquateClosure] = []
 
     public mutating func includeEquatableKeyPath<KeyType>(_ keyPath: KeyPath<Root, KeyType>) where KeyType: Equatable {
-//        let closure: EquateClosure = { (lhs: Root, rhs: Root) -> Bool in
-//            return lhs[keyPath: keyPath] == rhs[keyPath: keyPath]
-//        }
-//        closures.append(closure)
-//        closures.append({ (lhs: Root, rhs: Root) -> Bool in
-//            return lhs[keyPath: keyPath] == rhs[keyPath: keyPath]
-//        })
-//        closures.append({ (lhs: Root, rhs: Root) -> Bool in
-//            let lhsValue: KeyType = lhs[keyPath: keyPath]
-//            let rhsValue: KeyType = rhs[keyPath: keyPath]
-//            return lhsValue == rhsValue
-//        })
         closures.append({ lhs, rhs in
             return lhs[keyPath: keyPath] == rhs[keyPath: keyPath]
         })
