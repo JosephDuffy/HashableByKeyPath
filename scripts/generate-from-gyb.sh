b844ca6218d5d95a698f704d73bd982de7b94153
Joseph Duffy <commits@josephduffy.co.uk>
2021-02-19 14:23:20 +0000

    Generate EquatableKeyPathCollectionBuilder

--- scripts/generate-from-gyb.sh
+++ scripts/generate-from-gyb.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 EquatableKeyPathCollectionBuilder_FILE="Sources/HashableByKeyPath/ResultBuilderAPI/EquatableKeyPathCollectionBuilder.swift"
+HashableKeyPathCollectionBuilder_FILE="Sources/HashableByKeyPath/ResultBuilderAPI/HashableKeyPathCollectionBuilder.swift"
 
 gyb --line-directive '' -o "$EquatableKeyPathCollectionBuilder_FILE" "$EquatableKeyPathCollectionBuilder_FILE.gyb"
+gyb --line-directive '' -o "$HashableKeyPathCollectionBuilder_FILE" "$HashableKeyPathCollectionBuilder_FILE.gyb"
