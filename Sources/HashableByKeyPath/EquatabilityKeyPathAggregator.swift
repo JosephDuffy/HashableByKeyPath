e75975e6080efa8c71473224b38c8bc3db4fad12
Joseph Duffy <commits@josephduffy.co.uk>
2021-09-27 15:43:47 +0100

    Remove unused type

--- Sources/HashableByKeyPath/EquatabilityKeyPathAggregator.swift
+++ Sources/HashableByKeyPath/EquatabilityKeyPathAggregator.swift
@@ -2,8 +2,6 @@ internal struct EquatabilityKeyPathAggregator<Root>: EquatableKeyPathConsumer {
 
     private typealias EquateClosure = (_ lhs: Root, _ rhs: Root) -> Bool
 
-    internal typealias KeyPathListener<KeyType: Equatable> = (_ keyPath: KeyPath<Root, KeyType>) -> Void
-
     private var closures: [EquateClosure] = []
 
     internal mutating func addEquatableKeyPath<KeyType>(_ keyPath: KeyPath<Root, KeyType>) where KeyType: Equatable {
