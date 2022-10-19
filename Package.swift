bfb92e439b0cfaa53d08bd10c74043de51fa540e
Joseph Duffy <commits@josephduffy.co.uk>
2022-10-19 20:27:28 +0100

    Generate result builders via Sourcery

--- Package.swift
+++ Package.swift
@@ -6,6 +6,9 @@ let package = Package(
     products: [
         .library(name: "HashableByKeyPath", targets: ["HashableByKeyPath"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/krzysztofzablocki/Sourcery.git", .revision("8aab07cbda5206a2625ff9736c065262d2804364")),
+    ],
     targets: [
         .target(name: "HashableByKeyPath"),
         .testTarget(name: "HashableByKeyPathTests", dependencies: ["HashableByKeyPath"]),
