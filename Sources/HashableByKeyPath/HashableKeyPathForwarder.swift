21f9a9adf87cb6809b7e08603f3d11f6e8a6de74
Joseph Duffy <commits@josephduffy.co.uk>
2022-10-19 19:32:18 +0100

    Allow functions to be inlined

--- Sources/HashableByKeyPath/HashableKeyPathForwarder.swift
+++ Sources/HashableByKeyPath/HashableKeyPathForwarder.swift
@@ -1,5 +1,4 @@
 internal struct HashableKeyPathForwarder<Root, Consumer: EquatableKeyPathConsumer>: HashableKeyPathConsumer where Consumer.Root == Root {
-
     internal typealias KeyPathListener<KeyType: Equatable> = (_ keyPath: KeyPath<Root, KeyType>) -> Void
 
     internal var equatableKeyPathConsumer: Consumer
@@ -8,12 +7,13 @@ internal struct HashableKeyPathForwarder<Root, Consumer: EquatableKeyPathConsume
         self.equatableKeyPathConsumer = equatableKeyPathConsumer
     }
 
+    @inlinable
     internal mutating func addHashableKeyPath<KeyType>(_ keyPath: KeyPath<Root, KeyType>) where KeyType: Hashable {
         equatableKeyPathConsumer.addEquatableKeyPath(keyPath)
     }
 
+    @inlinable
     internal mutating func addCustomEquator<KeyType>(forKeyPath keyPath: KeyPath<Root, KeyType>, equator: @escaping (KeyType, KeyType) -> Bool) where KeyType: Hashable {
         equatableKeyPathConsumer.addCustomEquator(forKeyPath: keyPath, equator: equator)
     }
-
 }
