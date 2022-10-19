55a7e822df002ec403c133f17565b3e84885606a
Joseph Duffy <commits@josephduffy.co.uk>
2022-10-19 18:53:48 +0100

    Improve performance of hashing
    Roughly 2x faster by removing the need to store closures

---
+++ Tests/HashableByKeyPathTests/HashableByKeyPathTests.swift
@@ -0,0 +1,51 @@
+import XCTest
+import HashableByKeyPath
+
+final class HashableByKeyPathTests: XCTestCase {
+    func testHashPerformance() {
+        final class Foo: HashableByKeyPath {
+            static func addHashableKeyPaths<Consumer>(to consumer: inout Consumer) where Consumer.Root == Foo, Consumer: HashableKeyPathConsumer {
+                consumer.addHashableKeyPath(\.propertyA)
+                consumer.addHashableKeyPath(\.propertyB)
+                consumer.addHashableKeyPath(\.propertyC)
+                consumer.addHashableKeyPath(\.propertyD)
+                consumer.addHashableKeyPath(\.propertyE)
+                consumer.addHashableKeyPath(\.propertyF)
+                consumer.addHashableKeyPath(\.propertyG)
+                consumer.addHashableKeyPath(\.child)
+            }
+
+            let propertyA: String
+            let propertyB: String
+            let propertyC: String
+            let propertyD: String
+            let propertyE: String
+            let propertyF: String
+            let propertyG: String
+            private let child: Foo?
+
+            init(level: Int = 0) {
+                self.propertyA = "propertyA\(level)"
+                self.propertyB = "propertyB\(level)"
+                self.propertyC = "propertyC\(level)"
+                self.propertyD = "propertyD\(level)"
+                self.propertyE = "propertyE\(level)"
+                self.propertyF = "propertyF\(level)"
+                self.propertyG = "propertyG\(level)"
+                if level >= 5 {
+                    child = nil
+                } else {
+                    child = Foo(level: level + 1)
+                }
+            }
+        }
+
+        let foo1 = Foo()
+
+        measure {
+            for _ in 0..<100 {
+                _ = foo1.hashValue
+            }
+        }
+    }
+}
