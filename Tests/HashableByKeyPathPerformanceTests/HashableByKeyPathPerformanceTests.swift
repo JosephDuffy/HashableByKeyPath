ecf49f6ba6414f006276d6dbaed8f223caec05c6
Joseph Duffy <commits@josephduffy.co.uk>
2022-12-10 13:16:41 +0000

    Move classes to file-level

--- Tests/HashableByKeyPathPerformanceTests/HashableByKeyPathPerformanceTests.swift
+++ Tests/HashableByKeyPathPerformanceTests/HashableByKeyPathPerformanceTests.swift
@@ -3,43 +3,6 @@ import HashableByKeyPath
 
 final class HashableByKeyPathPerformanceTests: XCTestCase {
     func testHashPerformance() {
-        final class Foo: HashableByKeyPath {
-            static func addHashableKeyPaths<Consumer>(to consumer: inout Consumer) where Consumer.Root == Foo, Consumer: HashableKeyPathConsumer {
-                consumer.addHashableKeyPath(\.propertyA)
-                consumer.addHashableKeyPath(\.propertyB)
-                consumer.addHashableKeyPath(\.propertyC)
-                consumer.addHashableKeyPath(\.propertyD)
-                consumer.addHashableKeyPath(\.propertyE)
-                consumer.addHashableKeyPath(\.propertyF)
-                consumer.addHashableKeyPath(\.propertyG)
-                consumer.addHashableKeyPath(\.child)
-            }
-
-            let propertyA: String
-            let propertyB: String
-            let propertyC: String
-            let propertyD: String
-            let propertyE: String
-            let propertyF: String
-            let propertyG: String
-            private let child: Foo?
-
-            init(level: Int = 0) {
-                self.propertyA = "propertyA\(level)"
-                self.propertyB = "propertyB\(level)"
-                self.propertyC = "propertyC\(level)"
-                self.propertyD = "propertyD\(level)"
-                self.propertyE = "propertyE\(level)"
-                self.propertyF = "propertyF\(level)"
-                self.propertyG = "propertyG\(level)"
-                if level >= 5 {
-                    child = nil
-                } else {
-                    child = Foo(level: level + 1)
-                }
-            }
-        }
-
         let foo1 = Foo()
 
         measure {
@@ -50,43 +13,6 @@ final class HashableByKeyPathPerformanceTests: XCTestCase {
     }
 
     func testEqualityPerformance() {
-        final class Foo: HashableByKeyPath {
-            static func addHashableKeyPaths<Consumer>(to consumer: inout Consumer) where Consumer.Root == Foo, Consumer: HashableKeyPathConsumer {
-                consumer.addHashableKeyPath(\.propertyA)
-                consumer.addHashableKeyPath(\.propertyB)
-                consumer.addHashableKeyPath(\.propertyC)
-                consumer.addHashableKeyPath(\.propertyD)
-                consumer.addHashableKeyPath(\.propertyE)
-                consumer.addHashableKeyPath(\.propertyF)
-                consumer.addHashableKeyPath(\.propertyG)
-                consumer.addHashableKeyPath(\.child)
-            }
-
-            let propertyA: String
-            let propertyB: String
-            let propertyC: String
-            let propertyD: String
-            let propertyE: String
-            let propertyF: String
-            var propertyG: String
-            let child: Foo?
-
-            init(level: Int = 0) {
-                self.propertyA = "propertyA\(level)"
-                self.propertyB = "propertyB\(level)"
-                self.propertyC = "propertyC\(level)"
-                self.propertyD = "propertyD\(level)"
-                self.propertyE = "propertyE\(level)"
-                self.propertyF = "propertyF\(level)"
-                self.propertyG = "propertyG\(level)"
-                if level >= 5 {
-                    child = nil
-                } else {
-                    child = Foo(level: level + 1)
-                }
-            }
-        }
-
         let foo1 = Foo()
         let foo2 = Foo()
 
@@ -94,6 +20,43 @@ final class HashableByKeyPathPerformanceTests: XCTestCase {
             for _ in 0..<1000 {
                 _blackHole(foo1 == foo2)
             }
+        }
+    }
+}
+
+private final class Foo: HashableByKeyPath {
+    static func addHashableKeyPaths<Consumer>(to consumer: inout Consumer) where Consumer.Root == Foo, Consumer: HashableKeyPathConsumer {
+        consumer.addHashableKeyPath(\.propertyA)
+        consumer.addHashableKeyPath(\.propertyB)
+        consumer.addHashableKeyPath(\.propertyC)
+        consumer.addHashableKeyPath(\.propertyD)
+        consumer.addHashableKeyPath(\.propertyE)
+        consumer.addHashableKeyPath(\.propertyF)
+        consumer.addHashableKeyPath(\.propertyG)
+        consumer.addHashableKeyPath(\.child)
+    }
+
+    let propertyA: String
+    let propertyB: String
+    let propertyC: String
+    let propertyD: String
+    let propertyE: String
+    let propertyF: String
+    let propertyG: String
+    private let child: Foo?
+
+    init(level: Int = 0) {
+        self.propertyA = "propertyA\(level)"
+        self.propertyB = "propertyB\(level)"
+        self.propertyC = "propertyC\(level)"
+        self.propertyD = "propertyD\(level)"
+        self.propertyE = "propertyE\(level)"
+        self.propertyF = "propertyF\(level)"
+        self.propertyG = "propertyG\(level)"
+        if level >= 5 {
+            child = nil
+        } else {
+            child = Foo(level: level + 1)
         }
     }
 }
